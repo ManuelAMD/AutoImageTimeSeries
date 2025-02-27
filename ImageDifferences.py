230c2b26a1e0881aa5d557afdd526fe468fde738
ManuelAMD <manuel_md123@hotmail.com>
2025-02-27 08:40:44 -0700

    Agregando diferencias en imagenes

--- ImageDifferences.py
+++ ImageDifferences.py
@@ -4,20 +4,21 @@ import app.common.load_imgs as li
 import cv2
 
 horizon = 12
-forecasts = np.load("Models/DiferencesForecast.npy")
-original = np.load("Models/DiferencesOriginal.npy")
-naive = np.load("Models/DiferencesNaive.npy")
+#forecasts = np.load("Models/DiferencesForecast6DroughtDataset_model_testing_1731702460.npy")
+forecasts = np.load("Models/DiferencesNaive6.npy")
+original = np.load("Models/DiferencesOriginal6.npy")
+#naive = np.load("Models/DiferencesNaive6.npy")
 
-naive = naive.reshape(naive.shape[:-1])
+#naive = naive.reshape(naive.shape[:-1])
 original = original.reshape(original.shape[:-1])
 forecasts = forecasts.reshape(forecasts.shape[:-1])
 
 forecasts = np.stack((forecasts,)*3, axis=-1)
 original = np.stack((original,)*3, axis=-1)
-naive = np.stack((naive,)*3, axis=-1)
+#naive = np.stack((naive,)*3, axis=-1)
 
 print("Datos procesados para comparar imágenes")
-print(naive.shape)
+#print(naive.shape)
 print(original.shape)
 print(forecasts.shape)
 
@@ -33,9 +34,9 @@ for i in range(len(original)):
 
     cv2.imshow('diff', result)
     cv2.waitKey()
-    cv2.imwrite("GeneratedImageComparation/DifferenceForecastTesis_t+{}.png".format(i), result)
+    cv2.imwrite("GeneratedImageComparation/DifferenceForecastArticulo_t+{}.png".format(i), result)
 
-for i in range(len(original)):
+"""for i in range(len(original)):
     diff = 255 - cv2.absdiff(naive[i], original[i])
     
     res = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
@@ -47,4 +48,4 @@ for i in range(len(original)):
 
     #cv2.imshow('diff', result)
     cv2.waitKey()
-    cv2.imwrite("GeneratedImageComparation/DifferenceTesisNaive_t+{}.png".format(i), result)
+    cv2.imwrite("GeneratedImageComparation/DifferenceArticuloNaive_t+{}.png".format(i), result)"""
