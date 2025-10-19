deef35384c8b838affb6eac0923e4783ebffe553
ManuelAMD <manuel_md123@hotmail.com>
2025-10-19 14:07:19 -0700

    Fixing multi-step estimation images

--- ImageDifferences.py
+++ ImageDifferences.py
@@ -72,16 +72,17 @@ horizon = 12
 #original = np.load("Models/DiferencesOriginal5.npy")
 
 #Mejor Sequia ViViT
-forecasts = np.load("Models/DiferencesForecast4DroughtDataset_model_testing_1760123802.npy")
-original = np.load("Models/DiferencesOriginal4.npy")
+#forecasts = np.load("Models/DiferencesForecast4DroughtDataset_model_testing_1760123802.npy")
+#original = np.load("Models/DiferencesOriginal4.npy")
 
 #Mejor Sequia Recursiva
-forecasts = np.load("Models/DiferencesForecast10DroughtDataset_model_testing_1759339807.npy")
-original = np.load("Models/DiferencesOriginal10.npy")
+#forecasts = np.load("Models/DiferencesForecast10DroughtDataset_model_testing_1759339807.npy")
+#original = np.load("Models/DiferencesOriginal10.npy")
+#naive = np.load("Models/DiferencesNaive10.npy")
 
 #Mejor Sequia Directa
-#forecasts = np.load("Models/DiferencesForecast3DroughtDataset_model_testing_1760644102.npy")
-#original = np.load("Models/DiferencesOriginal3.npy")
+forecasts = np.load("Models/DiferencesForecast3DroughtDataset_model_testing_1760644102.npy")
+original = np.load("Models/DiferencesOriginal3.npy")
 
 #Mejor SPI ConvLSTM
 #forecasts = np.load("Models/DiferencesForecast5SPIDataset_model_testing_1759392299.npy")
@@ -131,12 +132,12 @@ for i in range(len(original)):
     #cv2.imwrite("GeneratedImageComparation/DifferenceSequiaConvLSTM_t+{}.png".format(i), result)
     #cv2.imwrite("GeneratedImageComparation/DifferenceSequiaMultiCNN_t+{}.png".format(i), result)
     #cv2.imwrite("GeneratedImageComparation/DifferenceSequiaViViT_t+{}.png".format(i), result)
-    cv2.imwrite("GeneratedImageComparation/DifferenceSequiaRecursiva_t+{}.png".format(i), result)
-    #cv2.imwrite("GeneratedImageComparation/DifferenceSequiaDirecta_t+{}.png".format(i), result)
+    #cv2.imwrite("GeneratedImageComparation/DifferenceSequiaRecursiva_t+{}.png".format(i), result)
+    cv2.imwrite("GeneratedImageComparation/DifferenceSequiaDirecta_t+{}.png".format(i), result)
     #cv2.imwrite("GeneratedImageComparation/DifferenceSPIConvLSTM_t+{}.png".format(i), result)
 
-
-"""for i in range(len(original)):
+"""
+for i in range(len(original)):
     diff = 255 - cv2.absdiff(naive[i], original[i])
     
     res = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
@@ -148,4 +149,4 @@ for i in range(len(original)):
 
     #cv2.imshow('diff', result)
     cv2.waitKey()
-    cv2.imwrite("GeneratedImageComparation/DifferenceArticuloNaive_t+{}.png".format(i), result)"""
+    cv2.imwrite("GeneratedImageComparation/DifferenceTesisNaive_t+{}.png".format(i), result)"""
